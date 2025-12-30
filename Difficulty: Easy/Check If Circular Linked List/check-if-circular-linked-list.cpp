3dfc12798ea43fc09d6d5f59ca17006b05638daf
axiver-student <jaxiver377@gmail.com>
2025-12-30 21:49:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check If Circular Linked List/check-if-circular-linked-list.cpp
@@ -0,0 +1,30 @@
+/* Link list Node
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+*/
+
+/* Should return true if linked list is circular, else false */
+class Solution {
+  public:
+    bool isCircular(Node *head) {
+        // Your code here
+        if (head == NULL) return true;
+        Node *temp=head;
+        while(temp!=NULL){
+            temp=temp->next;
+            if(temp==head){
+                return true;
+            }
+        }
+        return false;
+    }
+};
