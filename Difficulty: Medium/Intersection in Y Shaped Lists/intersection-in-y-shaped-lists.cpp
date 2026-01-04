6870547204d2b0c50100e7153c7f2e6549d79cba
Soumya_jain <jaxiver377@gmail.com>
2026-01-04 20:12:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Intersection in Y Shaped Lists/intersection-in-y-shaped-lists.cpp
+++ Difficulty: Medium/Intersection in Y Shaped Lists/intersection-in-y-shaped-lists.cpp
@@ -13,26 +13,39 @@ public:
 
 class Solution {
   public:
+  
+    int length(Node* head1){
+        Node *temp=head1;
+        int count=0;
+        while(temp!=NULL){
+            count++;
+            temp=temp->next;
+        }
+        return count;
+    }
     Node* intersectPoint(Node* head1, Node* head2) {
-        //  Code Here
-        unordered_set<Node*> st;
-
-        // store addresses of first list
-        Node* temp = head1;
-        while (temp != NULL) {
-            st.insert(temp);
-            temp = temp->next;
-        }
-
-        // check in second list
-        temp = head2;
-        while (temp != NULL) {
-            if (st.find(temp) != st.end()) {
-                return temp;   // intersection point
-            }
-            temp = temp->next;
-        }
-
-        return NULL;
+       
+       int l1=length(head1);
+       int l2=length(head2);
+       Node *temp1=head1;
+       Node *temp2=head2;
+       if(l1>l2){
+           int d = l1-l2;
+           for(int i=1;i<=d;i++){
+               temp1=temp1->next;
+           }
+       }
+       else{
+           int d = l2-l1;
+           for(int i=1;i<=d;i++){
+               temp2=temp2->next;
+           }
+       }
+       while(temp1!=temp2){
+           temp1=temp1->next;
+           temp2=temp2->next;
+       }
+       return temp1;
+        
     }
 };
